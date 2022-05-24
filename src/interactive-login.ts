b5fcf25347c3ec45342c468398caca09008872f5
Brock Taylor <13286385+brocktaylor7@users.noreply.github.com>
2022-05-24 15:58:33 -0700

    stability improvments

--- src/interactive-login.ts
+++ src/interactive-login.ts
@@ -17,30 +17,54 @@ export async function run() {
 
 async function authenticate(page: puppeteer.Page, attemptNumber: number = 1): Promise<void> {
     await page.goto('https://portal.azure.com');
+
+    if (authenticationSucceeded(page)) {
+        return;
+    }
     await page.waitForSelector('input[name="loginfmt"]');
     await page.type('input[name="loginfmt"]', process.env.SERVICE_ACCT_USER!);
     await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle0' }), page.keyboard.press('Enter')]);
 
     await page.waitForSelector('#FormsAuthentication');
     await page.click('#FormsAuthentication');
+    await page.waitForSelector('input[type="password"]');
     await page.type('input[type="password"]', process.env.SERVICE_ACCT_PASS!);
     await Promise.all([page.waitForNavigation({ waitUntil: 'networkidle0' }), page.keyboard.press('Enter')]);
+    const errorTextExists = await page.$eval('#errorText', () => true).catch(() => false);
+    if (errorTextExists) {
+        const errorText: string | null = await page.$eval('#errorText', (el) => el.textContent);
+        if (errorText !== undefined) {
+            throw new Error(`Authentication failed with error: ${errorText}`);
+        }
+    }
 
-    if (!page.url().match('^https://ms.portal.azure.com')) {
+    try {
+        await page.waitForSelector('#web-container', { timeout: 10000 });
+    } catch {
+        console.log('death');
+    }
+
+    if (!authenticationSucceeded(page)) {
         if (attemptNumber > 4) {
             console.error(`Attempted authentication ${attemptNumber} times and ultimately failed.`);
 
             return;
         }
-        const errorText: string | null = await page.$eval('#errorText', (el) => el.textContent);
-        if (errorText !== undefined) {
-            console.warn(`Authentication failed with error: ${errorText}`);
-        }
+
+        console.info('Retrying Authentication...');
         await authenticate(page, attemptNumber + 1);
 
         return;
     }
-    console.info('Authentication succeeded');
+}
+
+function authenticationSucceeded(page: puppeteer.Page) {
+    const currentUrl = page.url();
+    if (!currentUrl.match('^https://ms.portal.azure.com')) {
+        return false;
+    }
+    console.info('Authentication succeeded.');
+    return true;
 }
 
 run();
